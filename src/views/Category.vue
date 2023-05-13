f8c1e58b040b33b19589beb396cba390a3aa1a08
dannyxyxy <danny97k@gmail.com>
2023-05-14 03:04:47 +0900

    Start setting

--- src/views/Category.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-  <div class="maintext" :class="{ 'dark-mode': isDarkMode }">
-    //여기에 html 작업하세요. 파일복사하셔서 꼭 자신이 맡은 디자인상 페이지명으로 rename해주시고요. css 파일 이름은 페이지명과 동일하게 따로 작성하셔서 assets/style/view 폴더에 저장하셔서 임포트해주세요. 라우터 연결도 알아서 해주시면 센스쟁이.//
-  </div>
-</template>
