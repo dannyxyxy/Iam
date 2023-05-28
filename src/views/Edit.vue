4ff8bf8a215a5320e1b1c071ee56b488c5804aa8
sangwoong1 <116338155+sangwoong1@users.noreply.github.com>
2023-05-28 13:22:28 +0900

    Update Edit.vue

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,5 +1,44 @@
-<script setup></script>
+<script>
+import Editor from '@toast-ui/editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
 
-<template></template>
+export default {
+  mounted() {
+        const editor = new Editor({
+      el: this.$refs.editorRef,
+      initialEditType: 'markdown',
+      previewStyle: 'horizontal', 
+      height: '500px', 
+    });
+
+    //  초기 텍스트 설정
+    editor.setMarkdown('#  여기에 글을 작성하세요');
+
+    this.editor = editor;
+
+    // 에디터 내용 변경 이벤트 처리
+    editor.on('change', () => {
+      const content = editor.getMarkdown();
+      console.log('Editor content:', content);
+    });
+  },
+  methods: {
+    saveContent() {
+      const content = this.editor.getMarkdown();
+      // 여기에서 저장 로직을 구현하면 됩니다.
+      console.log('Saving content:', content);
+    },
+  },
+};
+</script>
+
+<template>
+  
+  <div class = editdiv>
+    <div ref="editorRef"></div>
+    <button class="savebutton" @click="saveContent">저장</button>
+  </div>
+ 
+</template>
 
 <style scoped></style>
