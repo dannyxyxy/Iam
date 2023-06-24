a09a0d8a55105eadda32d913d15345e6a3affa85
cl0ud <148cl0ud@gmail.com>
2023-06-24 21:16:49 +0900

    search 수정

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,54 +1,62 @@
 <template>
   <div class="maintext">
     <div class="container">
-    <div class="search">
-      <input
-        type="text"
-        placeholder="검색어를 입력하세요."
-        @input="data.searchQuery = $event.target.value"
-        @submit.prevent
-        @keydown.enter.prevent="search"
-      />
-      <div class="search_img">
-        <button
-          @click="search"
-          style="background-color: transparent; border: none; padding: 0">
-          <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"/>
-        </button>
+      <div class="search">
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요."
+          @input="data = $event.target.value"
+          @submit.prevent
+          @keydown.enter.prevent="search"
+        />
+        <div class="search_img">
+          <button
+            @click="search"
+            style="background-color: transparent; border: none; padding: 0"
+          >
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"
+            />
+          </button>
+        </div>
       </div>
-    </div>
 
-    <div class="search-results">
-      <h2 > 검색 결과</h2>
+      <div class="search-results">
+        <h2>검색 결과</h2>
         <ul v-if="searchResults.length > 0">
           <li v-for="item in searchResults" class="search-result">
-              
-              <div class = "box">
-                <router-link :to="{ name: 'PostMain', query: { id: item._id } }">
-                  <div class="box-img"></div>
-                </router-link>
+            <div class="box">
+              <router-link :to="{ name: 'PostMain', query: { id: item._id } }">
+                <div class="box-img"></div>
+              </router-link>
 
-                <div class="box-summary">
-                    <li><div class="board-title">
-                      <h3>{{ item.boardTitle }}</h3> </div></li>
-                    <li><div class="board-contents">
-                      <p>{{ item.boardContents }}</p> </div></li>
-                    <div class="circle">
-                      <img :src="CONSTANTS.API_URL + item.profileImg" alt="" />
-                    </div>
-                    <div class="username">
-                      <span style="font-weight: lighter">by </span>
-                      <span style="font-weight: bold">{{ item.userName }}</span>
-                    </div>
+              <div class="box-summary">
+                <li>
+                  <div class="board-title">
+                    <h3>{{ item.boardTitle }}</h3>
+                  </div>
+                </li>
+                <li>
+                  <div class="board-contents">
+                    <p>{{ item.boardContents }}</p>
+                  </div>
+                </li>
+                <div class="circle">
+                  <img :src="CONSTANTS.API_URL + item.profileImg" alt="" />
                 </div>
-
+                <div class="username">
+                  <span style="font-weight: lighter">by </span>
+                  <span style="font-weight: bold">{{ item.userName }}</span>
+                </div>
               </div>
+            </div>
           </li>
         </ul>
-        <div v-show="searchResults.length === 0 && data.searchQuery.trim() !== ''">
-          검색 결과가 없습니다.</div>
+        <div v-show="searchResults.length === 0 && data !== ''">
+          검색 결과가 없습니다.
         </div>
-  </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -58,54 +66,41 @@ import { ref } from "vue";
 import { apiClient } from "../utils/axios.js";
 import router from "../router/index.js";
 
-
 export default {
-  name: 'Search',
+  name: "Search",
   computed: {
     CONSTANTS() {
       return CONSTANTS;
     },
   },
 
-
   setup() {
-    const data = ref({
-      searchQuery: "",
-    });
+    const data = ref("");
     const searchResults = ref([]);
-    const hasSearchResults = ref(false);
 
     const search = async () => {
-      await apiClient("board/search", data.value)
+      await apiClient("board/search", { searchQuery: data.value })
         .then((r) => {
-          console.log(r);
           searchResults.value = r.data;
-          if (searchResults.value.length === 0) {
-            window.alert("검색 결과가 없습니다.");
-          }
         })
         .catch((e) => {
           console.log(e);
-          console.log("검색 결과가 없습니다.");
         });
-    }; 
+    };
 
     return {
       data,
       searchResults,
-      hasSearchResults,
       search,
     };
-  }, 
+  },
 };
-
 </script>
 
 <style>
-
 .search {
   position: relative;
-  width: 100%; 
+  width: 100%;
   margin-bottom: 20px;
 }
 .search input {
@@ -124,9 +119,7 @@ export default {
 }
 
 li {
-    float: left;
-    margin-right: 20pxs;
-
+  float: left;
+  margin-right: 20pxs;
 }
-
 </style>
