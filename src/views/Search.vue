9ecc1ad05bfad8646b2178c4ff6057e80502c43b
cl0ud <148cl0ud@gmail.com>
2023-06-27 01:45:07 +0900

    search css 수정

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,65 +1,3 @@
-<template>
-  <div class="maintext">
-    <div class="container">
-      <div class="search">
-        <input
-          type="text"
-          placeholder="검색어를 입력하세요."
-          @input="data = $event.target.value"
-          @submit.prevent
-          @keydown.enter.prevent="search"
-        />
-        <div class="search_img">
-          <button
-            @click="search"
-            style="background-color: transparent; border: none; padding: 0"
-          >
-            <img
-              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"
-            />
-          </button>
-        </div>
-      </div>
-
-      <div class="search-results">
-        <h2>검색 결과</h2>
-        <ul v-if="searchResults.length > 0">
-          <li v-for="item in searchResults" class="search-result">
-            <div class="box">
-              <router-link :to="{ name: 'PostMain', query: { id: item._id } }">
-                <div class="box-img"></div>
-              </router-link>
-
-              <div class="box-summary">
-                <li>
-                  <div class="board-title">
-                    <h3>{{ item.boardTitle }}</h3>
-                  </div>
-                </li>
-                <li>
-                  <div class="board-contents">
-                    <p>{{ item.boardContents }}</p>
-                  </div>
-                </li>
-                <div class="circle">
-                  <img :src="CONSTANTS.API_URL + item.profileImg" alt="" />
-                </div>
-                <div class="username">
-                  <span style="font-weight: lighter">by </span>
-                  <span style="font-weight: bold">{{ item.userName }}</span>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
-        <div v-show="searchResults.length === 0 && data !== ''">
-          검색 결과가 없습니다.
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script>
 import { CONSTANTS } from "../utils/constants.js";
 import { ref } from "vue";
@@ -82,6 +20,11 @@ export default {
       await apiClient("board/search", { searchQuery: data.value })
         .then((r) => {
           searchResults.value = r.data;
+          for (let item in r.data) {
+            r.data[item].color = `hsl(${
+              parseInt(Math.random() * 24, 10) * 15
+            }, 16%, 75%)`;
+          }
         })
         .catch((e) => {
           console.log(e);
@@ -96,6 +39,75 @@ export default {
   },
 };
 </script>
+
+<template>
+  <div class="maintext">
+    <div class="container">
+      <div class="search">
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요."
+          @input="data = $event.target.value"
+          @submit.prevent
+          @keydown.enter.prevent="search"
+        />
+        <div class="search_img">
+          <button
+            @click="search"
+            style="background-color: transparent; border: none; padding: 0"
+          >
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png"
+              alt=""
+            />
+          </button>
+        </div>
+      </div>
+
+      <div class="search-results">
+        <h2>검색 결과</h2>
+        <div class="container-box" v-if="searchResults.length > 0">
+          <div class="box" v-for="item in searchResults" :key="item._id">
+            <router-link :to="{ name: 'PostMain', query: { id: item._id } }">
+              <div class="box-img" :style="`background-color: ${item.color}`" />
+            </router-link>
+            <div class="box-summary">
+              <div>
+                <div class="board-title">
+                  {{ item.boardTitle }}
+                </div>
+              </div>
+              <div>
+                <div class="board-contents">
+                  {{ item.boardContents }}
+                </div>
+              </div>
+              <div class="circle">
+                <img
+                  v-if="item.profileImg !== null || undefined"
+                  :src="CONSTANTS.API_URL + item.profileImg"
+                  alt=""
+                />
+                <div
+                  v-else
+                  class="exchangeImg"
+                  :style="`background-color: ${item.color}`"
+                />
+              </div>
+              <div class="username">
+                <span style="font-weight: lighter">by </span>
+                <span style="font-weight: bold">{{ item.userName }}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-show="searchResults.length === 0 && data !== ''">
+          검색 결과가 없습니다.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style>
 .search {
@@ -117,9 +129,9 @@ export default {
   right: 20px;
   margin: 0;
 }
-
-li {
-  float: left;
-  margin-right: 20px;
+.container-box {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
 }
 </style>
