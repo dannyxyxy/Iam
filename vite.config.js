e0c5b4202b1825c37c1ade3b6fbff4f11ffbc915
cl0ud <148cl0ud@gmail.com>
2023-07-01 07:35:44 +0900

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: { chunkSizeWarningLimit: 1600 },
+  alias: {
+    "~": path.resolve(__dirname, "node_modules"),
+    "@": path.resolve(__dirname, "src"),
+  },
+});
