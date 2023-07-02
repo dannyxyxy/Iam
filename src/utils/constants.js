bd24645b25fdde944ced992125b070532b863f79
cl0ud <80829186+hcl0ud@users.noreply.github.com>
2023-07-02 14:49:55 +0900

    Delete src/utils directory

--- src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-import { KEY_LIST } from "./constants-key.js";
-
-export const CONSTANTS = {
-  KEY_LIST: KEY_LIST,
-  API_URL: "http://cl0ud148.iptime.org:4000/",
-  // API_URL: "http://localhost:4000/",
-  // FILE_SERVER: "http://148cl0ud.iptime.org:3002/",
-  DEVELOP: true, //developer
-};
